e294e8fc129566f23e57249497b11756bdfe3910
funbiscuit <kokurinsv@gmail.com>
2021-02-24 19:29:32 +0300

    Initial commit
    Base javafx project which is built to both fat-jar and native executable

---
+++ src/main/java/com/funbiscuit/jfx/curvedetect/Launcher.java
@@ -0,0 +1,10 @@
+package com.funbiscuit.jfx.curvedetect;
+
+/**
+ * Separate launcher is required for fat-jar to work
+ */
+public class Launcher {
+    public static void main(String[] args) {
+        Main.main(args);
+    }
+}
